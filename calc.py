79a8c114ae68f9e55ba398427bde7e18053fc1ff
Senna <sennatitcomb@gmail.com>
2021-04-15 18:42:39 -0700

    print values

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
 def calc(a, b):
     sum = a + b
-    #print(sum)
+    print(sum)
     difference = a - b
+    print(difference)
     multiply = a * b
+    print(multiply)
     divide = a / b
+    print(divide)
 
 calc(int(input("Enter a number: ")), int(input("Enter a second number: ")))
